b32538bcc1e40e3908f42aad881aabf35a76187f
Alexey Kondratov <alex.lumir@gmail.com>
2018-11-13 15:18:02 +0300

    Travis-CI and Appveyor integration

---
+++ README.md
@@ -0,0 +1,9 @@
+# PostgreSQL mirror
+[![Travis-ci Status](https://travis-ci.com/ololobus/postgres.svg?branch=master)](https://travis-ci.com/ololobus/postgres)
+[![Appveyor Status](https://ci.appveyor.com/api/projects/status/f42li9kqurfc3gjt/branch/master?svg=true)](https://ci.appveyor.com/project/ololobus/postgres)
+
+Involves automatic builds of PRs/commits on Linux (Travis-CI) and Windows (Appveyor):
+  * Linux: full `make check-world`
+  * Windows: only build + `make check`
+
+For original PostgreSQL readme [refer to README file](README).
---
+++ buildsetup.pl
@@ -0,0 +1,42 @@
+# First part of postgres build.pl, just doesn't run msbuild
+
+use strict;
+
+use File::Copy;
+
+BEGIN
+{
+
+    chdir("../../..") if (-d "../msvc" && -d "../../../src");
+
+}
+
+use lib "src/tools/msvc";
+
+use Cwd;
+
+use Mkvcbuild;
+
+# buildenv.pl is for specifying the build environment settings
+# it should contain lines like:
+# $ENV{PATH} = "c:/path/to/bison/bin;$ENV{PATH}";
+
+if (-e "src/tools/msvc/buildenv.pl")
+{
+    do "src/tools/msvc/buildenv.pl";
+}
+elsif (-e "./buildenv.pl")
+{
+    do "./buildenv.pl";
+}
+
+copy("config.pl", "src/tools/msvc/config.pl");
+
+# set up the project
+our $config;
+do "config_default.pl";
+do "config.pl" if (-f "src/tools/msvc/config.pl");
+
+# print "PATH: $_\n" foreach (split(';',$ENV{PATH}));
+
+Mkvcbuild::mkvcbuild($config);
